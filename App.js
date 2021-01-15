e795c536f99c0586837b564363c6a2fa6b3ee301
vu tran <vutran271@gmail.com>
2021-01-15 16:48:43 -0500

    Update from laptop

--- App.js
+++ App.js
@@ -10,13 +10,14 @@ import {
   TouchableOpacity,
   Button,
   SafeAreaView,
+  ActivityIndicator,
 } from "react-native";
 import bgImg from "./assets/background.jpg";
 import * as ImagePicker from "./node_modules/expo-image-picker";
 import logo from "./assets/zenLogo.jpeg";
 import { AntDesign } from '@expo/vector-icons'; 
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-
+import { MainPage } from "./src/zenContainer/MainPage"
 
 let op = async () => {
   let permission = await ImagePicker.getCameraPermissionsAsync();
@@ -34,44 +35,93 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      url: "https://zenspanail.com",
+      url: "https://zenspanail.com/", 
+      visible : false,
+      logoVisible : true,
     }
   }
-  onNavStateChange = (NavState) => {
-    console.log(NavState);
-    if(NavState.cangoback){
-      this.setState(this.state.url, ()=>{ this.state.url = this.state.url})
+
+  navStateChange = (navState) => {
+    console.log(navState);
+    if(navState.url === this.state.url) {
+      this.setState({logoVisible:true})
     }
+    else{
+      this.setState({logoVisible:false})
+    }
+  }
+
+  showLoading = () => {
+    this.setState({visible: true})
+  }
+  hideLoading = () => {
+    this.setState({visible:false})
+  }
+
+
+  goToHomePage = () => {
+      const redirectTo = 'window.location = "' + this.state.url + '"';
+      this.webview.injectJavaScript(redirectTo);
+      this.setState({logoVisible:true});
+  }
+
+  onNavStateChange = () => {
+
   }
 
   login = true ? "account-arrow-left" : "account-arrow-right";
 
   render() {
+    const ActivityIndicatorElement = () => {
+      return (
+        <View style={styles.activityIndicatorStyle}>
+          <ActivityIndicator
+            color="green"
+            size="large"
+          />
+        </View>
+      );
+    };
     return (
       <SafeAreaView style={ styles.MainContainer }>
-        <WebView
-        ref={(ref) => {
-          (this.webview = ref);
-        }}
-        source={{ uri: this.state.url }}
-        onNavigationStateChange={this.onNavStateChange} >
-        </WebView>
-        <View style={styles.menu}>
-        <TouchableOpacity onPress={ this.onNavStateChange}>
-         <AntDesign name="home" size={35} color="grey" />       
-        </TouchableOpacity>
-        <MaterialCommunityIcons name= {this.login} size={35} color="grey" /> 
-        <MaterialCommunityIcons name= {this.login} size={35} color="grey" /> 
+        <View style = { styles.container}>
+          <MainPage />
         </View>
+        
+        {/* // LOGO SECTION */}
+        {this.state.logoVisible ? <View style={styles.LogoTitle}>
+          <Image style={ styles.logo } source = { logo }></Image>
+        </View> : null}
+
+        <View style={styles.menu}>
+          <TouchableOpacity onPress={ this.goToHomePage}>
+          <AntDesign name="home" size={35} color="grey" />       
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <MaterialCommunityIcons name= {this.login} size={35} color="grey" /> 
+          </TouchableOpacity>
+         </View>     
+        {this.state.visible ? <ActivityIndicatorElement /> : null}
       </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  LogoTitle : {
+    position:"absolute",
+    bottom:650,
+    backgroundColor: null,
+    color: "red",
+    flex:0.09,
+    width:"100%",
+    height:60,
+    width:"50%",
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
   container: {
     flex:1,
-    marginTop:8
   },
   menu: {
     flex:0.09,
@@ -87,11 +137,23 @@ const styles = StyleSheet.create({
   logo: {
     width: 80,
     height: 60,
-    marginTop:5
   },
   MainContainer: {
     flex: 1,
     backgroundColor: "green",
     flexDirection:"column"
+  },
+  activityIndicatorStyle: {
+    flex: 1,
+    position: 'absolute',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
   },
 });
